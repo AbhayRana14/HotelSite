7a461c2efa004759c582b2cd3073e1bfc151b05e
Abhay Rana <14abhayrana@gmail.com>
2020-09-01 18:16:14 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar ,NavbarBrand } from 'reactstrap';
+import { DISHES } from './shared/dishes';
+import Menu from './components/MenuComponent';
 import './App.css';
 
-function App() {
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      dishes : DISHES
+    }
+  }
+render(){
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -11,8 +20,10 @@ function App() {
         <NavbarBrand href="/">Leela Hospitality</NavbarBrand>
         </div>
       </Navbar>
+      <Menu dishes={this.state.dishes}/>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,93 @@
+import React ,{ Component} from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+    CardTitle } from 'reactstrap';
+
+    class DishDetail extends Component{
+        constructor(props){
+            super(props);
+            this.state={
+
+            }
+        }
+
+
+
+    formatDate({ date }) {
+        return new Date(date).toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "short",
+          day: "numeric"
+        });
+      }
+
+
+    renderDish(dish) {
+        if (dish != null)
+            return(
+                <Card>
+                    <CardImg  src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return(
+                <div></div>
+            );
+
+    }
+    renderComments(cmnt){
+        if(cmnt != null){
+            const list =cmnt.map((cmnts)=>{
+                const date=cmnts.date
+
+            return(
+                <li key={cmnts.id} >
+                    <div>
+                        <p>{cmnts.comment}</p>
+                        <p>--{cmnts.author},{this.formatDate({date})}</p>
+                    </div>
+                </li>
+                );
+            })
+            return(
+                <div>
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {list}
+                    </ul>
+                </div>
+            )
+        }
+        else{
+            return(
+                <div><h6>There are no comments</h6></div>
+            )
+        }
+
+
+
+    }
+    render(){
+        
+          const {dishSelect} = this.props;
+        console.log(dishSelect);
+
+        return dishSelect?(
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderDish(dishSelect)}
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderComments(dishSelect.comments)}
+                </div>
+            </div>
+        ):(
+            <div></div>
+        )
+
+    }
+    }
+    export default DishDetail;
